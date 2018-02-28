b47513996bdcc60a159355897ea3bb8c095373e4
Brian Wright <briankw23@gmail.com>
2018-02-27 19:46:59 -0600

    completed exercise 1

--- arrays.js
+++ arrays.js
@@ -3,12 +3,14 @@ console.log("Testlkenfoerbk;sgnioerwngion")
 var a = [1, 2, 3];
 var b = a.join(' ');
 
-var test1 = "cat"
-var test2 = "racecar"
-var test3 = "bear"
-var test4 = "mom"
+var test1 = "cat";
+var test2 = "racecar";
+var test3 = "bear";
+var test4 = "mom";
 
-if (test1= test1.split("").reverse().join("")){
+var opp = test1.split('').reverse().join('');
+
+if (test1 === opp){
     console.log(true);
 } else{
     console.log(false);
